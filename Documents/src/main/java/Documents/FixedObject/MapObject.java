bfaa34295bc7176bf7801a7a1e89f397f60f8018
Ryan Martin <jrm22@sfu.ca>
2024-03-01 14:56:53 -0800

    First MapObject implementation
    Changes:
    + implemented private variables, getters, setters
    
    Needs improved documentation

---
+++ Documents/src/main/java/Documents/FixedObject/MapObject.java
@@ -0,0 +1,65 @@
+package com.Documents;
+
+public abstract class MapObject {
+    /**
+     * Private variable declaration for the MapObject abstract class
+     *      xPos for holding the x position
+     *      yPos for holding the y position
+     *      charType for ??
+     */
+    private int xPos;
+    private int yPos;
+    private int charType;
+
+    /**
+     * Getters for the positions of objects?
+     *
+     *
+     * Getter for xPos
+     */
+    public int getX() {
+        return xPos;
+    }
+
+    /**
+     * Getter for yPos
+     */
+    public int getY() {
+        return yPos;
+    }
+
+    /**
+     * Getter for charType
+     */
+    public int getCharType() {
+        return charType;
+    }
+
+    /**
+     * Setters for the positions of objects?
+     *
+     *
+     * Setter for xPos
+     */
+    public void setX(int xPos) {
+        this.xPos = xPos;
+    }
+
+    /**
+     * Setter for yPos
+     */
+    public void setY(int yPos) {
+        this.yPos = yPos;
+    }
+
+    /**
+     *  toString provides a string statement of all the attributes of the object.
+     *  Can be used for error checking?
+     */
+    @Override
+    public String toString() {
+        return "MapObject{" + "xPos=" + xPos + ", yPos=" + yPos + ", charType=" + charType + '}';
+    }
+
+
+}
