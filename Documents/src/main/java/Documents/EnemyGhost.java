2555971caf5c1b654621cf30428b3552ad75a85e
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-12 23:21:42 -0700

    Update EnemyGhost checkcollision method

--- Documents/src/main/java/Documents/EnemyGhost.java
+++ Documents/src/main/java/Documents/EnemyGhost.java
@@ -62,7 +62,9 @@ public class EnemyGhost extends DynamicCharacter {
     @Override
     public boolean checkCollision(MapObject other) {
         // Implement collision detection for enemy ghosts
-        
+        if (other instanceof EnemyGhost) {
+            return true;
+        }
         return false;
     }
 
