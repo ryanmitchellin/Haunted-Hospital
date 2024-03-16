6ab8ac574e768bf4a4a0f8bcb18e3ee73fa9a631
jeaohl <jeaohl@sfu.ca>
2024-03-15 21:09:55 -0700

    main for the game

---
+++ Documents/src/main/java/Documents/main/Main.java
@@ -0,0 +1,28 @@
+package Documents.main;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Color;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame ();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("CMPT 276 Project Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        //to make the window to be the sized fit of the preferred size and the layouts of
+        //its subcomponents GamePanel
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.gameStartThread();
+    }
+}
