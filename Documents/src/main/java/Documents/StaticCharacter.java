f646175188e1538a92e8f6a7bb6f5d257d40a617
jeaohl <jeaohl@sfu.ca>
2024-03-15 21:08:32 -0700

    changing into file Structure

--- Documents/src/main/java/Documents/StaticCharacter.java
+++
@@ -1,9 +0,0 @@
-package Documents;
-
-
-public abstract class StaticCharacter extends Character {
-    public StaticCharacter(int x, int y) {
-        super(x, y);
-    }
-
-}
---
+++ Documents/src/main/java/Documents/entity/Entity.java
@@ -0,0 +1,24 @@
+package Documents.entity;
+
+import java.awt.image.BufferedImage;
+import java.awt.geom.AffineTransform;
+import java.awt.Rectangle;
+
+public class Entity {
+	public int xPos;
+	public int yPos;
+	public int vel;
+
+	//for character image load
+	public BufferedImage upward1,upward2,downward1,downward2,leftward1,leftward2,rightward1,rightward2;
+	public String direction;
+	//for animation count for the movement
+	public int spriteCount = 0;
+	public int spriteNum = 1;
+
+
+	//collision for the tiles
+	public Rectangle detectionArea;
+	public boolean isCollision = false;
+
+}
