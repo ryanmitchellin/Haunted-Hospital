ce33f756cd5a2481d36689217bdadbe1226190a0
jeaohl <jeaohl@sfu.ca>
2024-03-17 04:08:34 -0700

    item collision check added

---
+++ Documents/src/main/java/Documents/main/CollisionCheck.java
@@ -0,0 +1,132 @@
+package Documents.main;
+
+import Documents.tile.TileFactory;
+import Documents.entity.Entity;
+
+
+public class CollisionCheck {
+	GamePanel gp;
+
+	public CollisionCheck(GamePanel gp) {
+		this.gp = gp;
+	}
+
+
+	public void tileCheck(Entity entity) {
+		int left = entity.wxPos + entity.detectionArea.x;
+		int right = entity.wxPos + entity.detectionArea.x + entity.detectionArea.width;
+		int top = entity.wyPos + entity.detectionArea.y;
+		int bottom = entity.wyPos + entity.detectionArea.y + entity.detectionArea.height;
+
+		int leftCol = left/gp.tileSize;
+		int rightCol = right/gp.tileSize;
+		int topRow = top/gp.tileSize;
+		int bottomRow = bottom/gp.tileSize;
+
+		int tile1, tile2;
+
+		switch(entity.direction) {
+		case "up":
+			topRow = (top - entity.vel)/gp.tileSize;
+			tile1 = gp.tileFactory.getTileMapNum(leftCol, topRow);
+			tile2 = gp.tileFactory.getTileMapNum(rightCol, topRow);
+			if(gp.tileFactory.getTile(tile1).collision || gp.tileFactory.getTile(tile2).collision) {
+			    entity.isCollision = true;
+			}
+			break;
+		case "down":
+			bottomRow = (bottom + entity.vel)/gp.tileSize;
+			tile1 = gp.tileFactory.getTileMapNum(leftCol, bottomRow);
+			tile2 = gp.tileFactory.getTileMapNum(rightCol, bottomRow);
+			if(gp.tileFactory.getTile(tile1).collision || gp.tileFactory.getTile(tile2).collision) {
+			    entity.isCollision = true;
+			}
+			break;
+		case "left":
+			leftCol = (left - entity.vel)/gp.tileSize;
+			tile1 = gp.tileFactory.getTileMapNum(leftCol, topRow);
+			tile2 = gp.tileFactory.getTileMapNum(leftCol, bottomRow);
+			if(gp.tileFactory.getTile(tile1).collision || gp.tileFactory.getTile(tile2).collision) {
+			    entity.isCollision = true;
+			}
+			break;
+		case "right":
+			rightCol = (right + entity.vel)/gp.tileSize;
+			tile1 = gp.tileFactory.getTileMapNum(rightCol, topRow);
+			tile2 = gp.tileFactory.getTileMapNum(rightCol, bottomRow);
+			if(gp.tileFactory.getTile(tile1).collision || gp.tileFactory.getTile(tile2).collision) {
+			    entity.isCollision = true;
+			}
+			break;
+		} 
+	}
+
+	public int objCheck(Entity entity, boolean mainChar) {
+		int index = 999;
+
+		for(int i = 0; i < gp.obj.length; i++) {
+			if(gp.obj[i] != null) {
+				//get Entity's detection area pos
+				entity.detectionArea.x = entity.wxPos + entity.detectionArea.x;
+				entity.detectionArea.y = entity.wyPos + entity.detectionArea.y;
+
+				//get the object's detection area pos
+				gp.obj[i].detectionArea.x = gp.obj[i].worldX + gp.obj[i].detectionArea.x;
+				gp.obj[i].detectionArea.y = gp.obj[i].worldY + gp.obj[i].detectionArea.y;
+
+				switch(entity.direction) {
+				case "up":
+					entity.detectionArea.y -= entity.vel;
+					if(entity.detectionArea.intersects(gp.obj[i].detectionArea)) {
+						if(gp.obj[i].isCollision == true) {
+							entity.isCollision = true;
+						}
+						if(mainChar == true) {
+							index = i;
+						}
+					}
+					break;
+				case "down":
+					entity.detectionArea.y += entity.vel;
+					if(entity.detectionArea.intersects(gp.obj[i].detectionArea)) {
+						if(gp.obj[i].isCollision == true) {
+							entity.isCollision = true;
+						}
+						if(mainChar == true) {
+							index = i;
+						}
+					}
+					break;
+				case "left":
+					entity.detectionArea.x -= entity.vel;
+					if(entity.detectionArea.intersects(gp.obj[i].detectionArea)) {
+						if(gp.obj[i].isCollision == true) {
+							entity.isCollision = true;
+						}
+						if(mainChar == true) {
+							index = i;
+						}
+					}
+					break;
+				case "right":
+					entity.detectionArea.x += entity.vel;
+					if(entity.detectionArea.intersects(gp.obj[i].detectionArea)) {
+						if(gp.obj[i].isCollision == true) {
+							entity.isCollision = true;
+						}
+						if(mainChar == true) {
+							index = i;
+						}
+					}
+					break;
+				}
+				entity.detectionArea.x = entity.detectionDefaultX;
+				entity.detectionArea.y = entity.detectionDefaultY;
+				gp.obj[i].detectionArea.x = gp.obj[i].detectionDefaultX;
+				gp.obj[i].detectionArea.y = gp.obj[i].detectionDefaultY;
+			}
+		}
+
+		return index;
+	}
+}
