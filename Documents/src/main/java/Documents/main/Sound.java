08f0b07a606bc70ff6bad3b7d81a975d0656f18f
jeaohl <jeaohl@sfu.ca>
2024-03-17 04:29:46 -0700

    sound play,loop,stop method added using Clip AudioSystem

--- Documents/src/main/java/Documents/main/Sound.java
+++ Documents/src/main/java/Documents/main/Sound.java
@@ -2,6 +2,8 @@ import Documents.main;
 
 import java.net.URL;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.AudioInputStream;
 
 public class Sound {
 	//used to open the audio file
@@ -11,7 +13,31 @@ public class Sound {
 	URL soundURL[] = new URL[20];
 
 	public Sound() {
-		soundURL[0] = getClass().getResource("/sound/");
+		soundURL[0] = getClass().getResource("/sound/map1darkSound.wav");
+		soundURL[1] = getClass().getResource("/sound/collectkey.wav");
+	}
+
+	//adding the audio and set if not able to load catch error
+	public void fileSet(int a) {
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+		}catch(Exception e) {
+
+		}
+	}
+
+	public void play() {
+		clip.start();
+	}
+
+	public void loop() {
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+
+	public void stop() {
+		clip.stop();
 	}
 
 }
