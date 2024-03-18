a464f681103d2b9347f85a183c57f70104ee486f
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-18 12:16:46 -0700

    Create javadoc for Tile.java

--- Documents/src/main/java/Documents/tile/Tile.java
+++ Documents/src/main/java/Documents/tile/Tile.java
@@ -2,8 +2,14 @@ package Documents.tile;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Represents a tile in the maze.
+ */
 public class Tile {
+	/** The image associated with the tile. */
 	public BufferedImage image;
+	
+    /** Indicates if the collision happened. */
 	public boolean collision = false;
 
 }
