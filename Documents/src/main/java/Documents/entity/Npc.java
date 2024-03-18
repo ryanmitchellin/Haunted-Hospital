57d057d52662734a8e27d5ccf9e7ff13e36a926e
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-18 01:20:57 -0700

    Add setAction method to let npc move randomly (AI implementation)

--- Documents/src/main/java/Documents/entity/Npc.java
+++ Documents/src/main/java/Documents/entity/Npc.java
@@ -22,4 +22,29 @@ public class Npc extends Entity {
     	rightward1 = setup("/npc/boy_right_1");
     	rightward2 = setup("/npc/boy_right_2");
 	}
+
+    public void setAction(){
+
+        actionLockCount++;
+        if (actionLockCount == 120){
+            Random random = new Random();
+            int i = random.nextInt(100)+1; //pick up num from 1 to 100
+
+            if (i <= 25) {
+                direction = "up";
+            }
+            if (i > 25 && i <= 50) {
+                direction = "down";
+            }
+            if (i > 50 && i <= 75) {
+                direction = "left";
+            }
+            if (i > 75 && i <= 100) {
+                direction = "right";
+            }
+
+            actionLockCount = 0;
+        }
+        
+    }
 }
