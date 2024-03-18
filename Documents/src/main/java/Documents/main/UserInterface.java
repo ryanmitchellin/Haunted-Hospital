8c32d3b7956105cfa9b2a99e085f2164800cb9a2
jrm22 <jrm22@sfu.ca>
2024-03-18 00:44:45 -0700

    added methods

--- Documents/src/main/java/Documents/main/UserInterface.java
+++ Documents/src/main/java/Documents/main/UserInterface.java
@@ -10,8 +10,9 @@ import java.text.DecimalFormat;
 
 public class UserInterface {
 	GamePanel gp;
+	Graphics2D g2; //maybe rename
 	Font arialFont_40,arialFont_80;
-	BufferedImage cardKeyImg;
+	//BufferedImage cardKeyImg; (commented out #13)
 
 	int msgCount = 0;
 
@@ -27,8 +28,8 @@ public class UserInterface {
 		this.gp = gp;
 		arialFont_40 = new Font("Arial", Font.PLAIN, 40);
 		arialFont_80 = new Font("Arial", Font.BOLD, 80);
-		KeyCard keyCard = new KeyCard(gp);
-		cardKeyImg = keyCard.img;
+		//KeyCard keyCard = new KeyCard(gp); //commented out #13
+		//cardKeyImg = keyCard.img; //commented out #13
 	}
 
 	public void displayMessage(String text) {
@@ -37,56 +38,27 @@ public class UserInterface {
 	}
 
 	public void draw(Graphics2D g2) {
-		if(gameDone == false) {
-			g2.setFont(arialFont_40);
-			g2.setColor(Color.white);
-			g2.drawImage(cardKeyImg, gp.tileSize/2, gp.tileSize/6, gp.tileSize, gp.tileSize, null);
-			g2.drawString(" x " + gp.mainCharacter.keyNum + " / 4", 74, 50);
-			gameTime += (double)1/60;
-			g2.drawString("Time:" + dFormat.format(gameTime), gp.tileSize*11, 50);
+		this.g2 = g2;
 
-			//displaying message
-			if(printMessage == true) {
-				g2.setFont(g2.getFont().deriveFont(20));
-				g2.drawString(msg,gp.tileSize,gp.tileSize * 5); 
+		g2.setFont(arial_40); //why is this wrong
+		g2.setColor(Color.white);
 
-				msgCount++;
-
-				if(msgCount > 120) {
-					msgCount = 0;
-					printMessage = false;
-				}
-			}
-		} else {
-			g2.setFont(arialFont_40);
-			g2.setColor(Color.white);
-			String text;
-			int textLength;
-			int x;
-			int y;
-
-			text = "You escaped from the Hospital!";
-			textLength = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
-			x = gp.screenWidth/2 - textLength/2;
-			y = gp.screenHeight/2;
-			g2.drawString(text,x,y);
-			//time of the play
-			text = "Your total play time is :" + dFormat.format(gameTime) + "!";
-			textLength = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
-			x = gp.screenWidth/2 - textLength/2;
-			y = gp.screenHeight/2 + (gp.tileSize*4);
-			g2.drawString(text,x,y);
-
-			g2.setFont(arialFont_80);
-			g2.setColor(Color.yellow);
-			text = "Congratulations!";
-			textLength = (int)g2.getFontMetrics().getStringBounds(text,g2).getWidth();
-			x = gp.screenWidth/2 - textLength/2;
-			y = gp.screenHeight/2 - (gp.tileSize*2);
-			g2.drawString(text,x,y);
-
-			gp.gameThread = null;
-
+		if (gp.gameState == gp.playState) {
+			// Do playState stuff later
+		}
+		if (gp.gameState == gp.pauseState) {
+			drawPauseScreen();
 		}
 	}
-}
+	public void drawPauseScreen() {
+
+		String text = "PAUSED";
+		int x;
+		int length = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+		x = gp.screenWidth/2 - length/2;
+		int y = gp.screenHeight/2;
+
+		g2.drawString(text, x, y);
+	}
+
+	}
