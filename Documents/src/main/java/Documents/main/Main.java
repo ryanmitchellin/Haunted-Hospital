6817624ded71362d9074868c055c1a55d941b544
jeaohl <jeaohl@sfu.ca>
2024-03-17 20:39:59 -0700

    added list of png

--- Documents/src/main/java/Documents/main/Main.java
+++ Documents/src/main/java/Documents/main/Main.java
@@ -1,5 +1,38 @@
 package Documents.main;
 
+/*
+    The png used for tiles were created by Jea Oh Lee (301351043)
+
+    List of created *.png used in tile:
+    voidspace
+    rightsideWallEnd
+    blueFloor
+    upWallRight
+    orangeWallRight
+    leftsideWallEnd
+    cornerRightBottomToUp
+    bottomWallCenter
+    connector1
+    connector2
+    blueWallCenter
+    blueWallLeft
+    upWallCen
+    roomFloor
+    leftsideWall
+    orangeWallCenter
+    blueWallRight
+    upWallLeft
+    orangeWallLeft
+    grayFloor
+    rightsideWallcornerLeftBottomToup
+
+    List of created *.png used in object:
+    door
+    stair
+    cardkey
+
+*/
+
 import javax.swing.JFrame;
 import java.awt.Dimension;
 
