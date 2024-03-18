b976cc5429d660adaffa2b5533b27f9f61d21a2d
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-18 12:09:00 -0700

    Create javadoc for Sound.java

--- Documents/src/main/java/Documents/main/Sound.java
+++ Documents/src/main/java/Documents/main/Sound.java
@@ -5,6 +5,10 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.AudioInputStream;
 
+/**
+ * The Sound class handles audio playback in the game.
+ * It loads and plays different audio files for various game events.
+ */
 public class Sound {
 	//used to open the audio file
 	Clip clip;
@@ -12,13 +16,19 @@ public class Sound {
 	//to store the path of the sound file
 	URL soundURL[] = new URL[20];
 
+	/**
+     * Constructs a Sound object and initializes the URLs for the sound files.
+     */
 	public Sound() {
 		soundURL[0] = getClass().getResource("/sound/map1.wav");
 		soundURL[1] = getClass().getResource("/sound/collectkey.wav");
 		soundURL[2] = getClass().getResource("/sound/doorOpen.wav");
 	}
 
-	//adding the audio and set if not able to load catch error
+	/**
+     * Loads the audio file specified by the index i.
+     * @param i The index of the sound file to load.
+     */
 	public void fileSet(int i) {
 		try {
 			AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
@@ -29,14 +39,23 @@ public class Sound {
 		}
 	}
 
+	/**
+     * Starts playing the loaded audio file.
+     */
 	public void play() {
 		clip.start();
 	}
 
+	/**
+     * Loops the loaded audio file continuously.
+     */
 	public void loop() {
 		clip.loop(Clip.LOOP_CONTINUOUSLY);
 	}
 
+	/**
+     * Stops playing the audio file.
+     */
 	public void stop() {
 		clip.stop();
 	}
