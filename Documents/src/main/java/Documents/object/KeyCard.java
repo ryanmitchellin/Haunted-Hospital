c8340323c34543d21d2ad6bd7e02ff8c859cbbbe
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-18 12:13:40 -0700

    Create javadoc for keycard.java

--- Documents/src/main/java/Documents/object/KeyCard.java
+++ Documents/src/main/java/Documents/object/KeyCard.java
@@ -5,10 +5,16 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
+/**
+ * Represents a keycard object in the maze.
+ */
 public class KeyCard extends ObjectFactory {
 	GamePanel gp;
 
-
+	/**
+     * Constructs a KeyCard object.
+	 * @param gp The GamePanel instance associate with key card object.
+     */
 	public KeyCard(GamePanel gp) {
 		this.gp = gp;
 		this.type = "keyCard";
