261d458e93f242fb5a0c9e4fae3cac486a362079
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-18 02:00:17 -0700

    Update drawsubwindow method

--- Documents/src/main/java/Documents/main/UserInterface.java
+++ Documents/src/main/java/Documents/main/UserInterface.java
@@ -65,14 +65,18 @@ public class UserInterface {
 		int x = gp.tileSize*2;
 		int y = gp.tileSize/2;
 		int width = gp.screenWidth - (gp.tileSize*4);
-		int height = gp.tileSize*5;
+		int height = gp.tileSize*4;
 
 		drawSubWindow(x, y, width, height);
 	}
 	public void drawSubWindow (int x, int y, int width, int height){
-		Color c = new Color(0,0,0);
+		Color c = new Color(0,0,0, 220);
 		g2.setColor(c);
 		g2.fillRoundRect(x, y, width, height, 35, 35);
+
+		c = new Color(255,255,255);
+		g2.setStroke(new BasicStroke(5));
+		g2.drawRoundRect(x+5, y+5, width-10, height-10, 25, 25);
 	}
 	public int getXCenterText(String text) {
 		int length = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
