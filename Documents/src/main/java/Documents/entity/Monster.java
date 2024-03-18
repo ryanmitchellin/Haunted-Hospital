7982e2af0f4854ac23072fc340137be00d8a45b3
Liam Cummings <lfc3@sfu.ca>
2024-03-18 15:07:58 -0700

    Created setMonsterImage
    Using npc sprite for now

--- Documents/src/main/java/Documents/entity/Monster.java
+++ Documents/src/main/java/Documents/entity/Monster.java
@@ -5,5 +5,24 @@ import Documents.main.GamePanel;
 public class Monster extends Entity{
     public Monster(GamePanel gp){
         super(gp);
+
     }
+
+    public void getMonsterImage(){
+        upward1 = setup("/npc/boy_up_1");
+    	upward2 = setup("/npc/boy_up_2");
+    	downward1 = setup("/npc/boy_down_1");
+    	downward2 = setup("/npc/boy_down_2");
+    	leftward1 = setup("/npc/boy_left_1");
+    	leftward2 = setup("/npc/boy_left_2");
+    	rightward1 = setup("/npc/boy_right_1");
+    	rightward2 = setup("/npc/boy_right_2");
+
+
+
+    }
+
+
+
+    
 }
