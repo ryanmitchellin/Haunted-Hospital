e6519d5c85dc8f62a85ab754e3e2780a2f54e820
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-18 01:18:29 -0700

    Add constructor for the npc class

--- Documents/src/main/java/Documents/entity/Npc.java
+++ Documents/src/main/java/Documents/entity/Npc.java
@@ -3,5 +3,11 @@ package entity;
 import main.GamePanel;
 
 public class Npc extends Entity {
+    public Npc (GamePanel gp) {
+        super(gp);
 
+        direction = "down";
+        speed = 1;
+        
+    }
 }
