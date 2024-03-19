54778462102655cf8ef8699f4e433d7a5a333059
jrm22 <jrm22@sfu.ca>
2024-03-18 19:47:57 -0700

    Added goal column and row variables

--- Documents/src/main/java/Documents/entity/Demon.java
+++ Documents/src/main/java/Documents/entity/Demon.java
@@ -19,10 +19,19 @@ public class Demon extends Monster {
     	leftward2 = setup("/npc/boy_left_2");
     	rightward1 = setup("/npc/boy_right_1");
     	rightward2 = setup("/npc/boy_right_2");
-
-
-
     }
+
+	public void setAction() {
+
+		if (onPath == true) {
+			int goalColumn = 12;
+			int goalRow = 9;
+			int goalColumn = (gp.mainCharacter.wxPos + gp.mainCharacter.detectionArea.x)/gp.tileSize;
+			int goalRow = (gp.mainCharacter.wyPos + gp.mainCharacter.detectionArea.y)/gp.tileSize;
+
+			
+		}
+	}
 
     
 
