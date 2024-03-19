974bcd691f1ec9b08de6411e0514e11751cc7d27
jrm22 <jrm22@sfu.ca>
2024-03-18 21:15:12 -0700

    Manual merge

--- Documents/src/main/java/Documents/entity/Demon.java
+++ Documents/src/main/java/Documents/entity/Demon.java
@@ -1,16 +1,30 @@
 package Documents.entity;
 
 import Documents.main.GamePanel;
+import java.awt.*;
+import java.util.Random;
 
 public class Demon extends Monster {
 
     public Demon(GamePanel gp) {
-        super(gp);
-       
+		super(gp);
+		//TODO Auto-generated constructor stub
+		direction = "down";
+		this.vel = 1;
+
+		getDemonImage();
+
+		detectionArea = new Rectangle();
+		detectionArea.x = 8;
+		detectionArea.y = 16;
+
+
+		detectionArea.width = 18;
+		detectionArea.height = 18;
     }
     
 
-    public void getDemonImageß(){
+    public void getDemonImage(){
         upward1 = setup("/npc/boy_up_1");
     	upward2 = setup("/npc/boy_up_2");
     	downward1 = setup("/npc/boy_down_1");
@@ -19,11 +33,59 @@ public class Demon extends Monster {
     	leftward2 = setup("/npc/boy_left_2");
     	rightward1 = setup("/npc/boy_right_1");
     	rightward2 = setup("/npc/boy_right_2");
-
-
-
     }
 
-    
+	public void setAction() {
 
+		if (onPath) {
+			int goalColumn = (gp.mainCharacter.wxPos + gp.mainCharacter.detectionArea.x) / gp.tileSize;
+			int goalRow = (gp.mainCharacter.wyPos + gp.mainCharacter.detectionArea.y) / gp.tileSize;
+
+			searchPath(goalColumn, goalRow);
+		} else {
+			actionLockCount++;
+
+			if (actionLockCount == 120) {
+				Random random = new Random();
+				int i = random.nextInt(100) + 1;
+
+				if (i <= 25) {
+					direction = "up";
+				}
+				if (i > 25 && i <= 50) {
+					direction = "down";
+				}
+				if (i > 50 && i <= 75) {
+					direction = "left";
+				}
+				if (i > 75 && i <= 100) {
+					direction = "right";
+				}
+				actionLockCount = 0;
+			}
+		}
+	}
+
+	@Override
+	public void update() {
+
+		super.update();
+
+		int mainX = Math.abs(wxPos - gp.mainCharacter.wxPos);
+		int mainY = Math.abs(wyPos - gp.mainCharacter.wyPos);
+		int tDist = (mainX + mainY)/gp.tileSize;
+
+		if (onPath == false && tDist < 500) {
+			int i = new Random().nextInt(100)+1;
+			if (i > 50) {
+				onPath = true;
+			}
+		}/**
+		 if (onPath == true && tDist > 20) {
+		 onPath = false;
+		 }
+		 */
+
+	}
+	
 }
