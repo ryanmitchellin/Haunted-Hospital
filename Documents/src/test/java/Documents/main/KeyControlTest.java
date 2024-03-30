36aba166deed87d14acfc849cf73d503e4deae0d
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-30 15:24:29 -0700

    Add test for upPressed in KeyControl

--- Documents/src/test/java/Documents/main/KeyControlTest.java
+++ Documents/src/test/java/Documents/main/KeyControlTest.java
@@ -15,4 +15,18 @@ public class KeyControlTest {
         keyControl = new KeyControl(gamePanel);
         gamePanel.gameState = gamePanel.playState;
     }
+
+    @Test
+    public void testUpKeyPressed() {
+        KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_UP, 'U');
+        keyControl.keyPressed(keyEvent);
+        assertTrue(keyControl.upPressed);
+    }
+
+    @Test
+    public void testUpKeyReleased() {
+        KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, 0, 0, KeyEvent.VK_UP, 'U');
+        keyControl.keyPressed(keyEvent);
+        assertFalse(keyControl.upPressed);
+    }
 }
