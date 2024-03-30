6ac06ef32251e31d019d5d5c0be0ff03957cd08d
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-03-30 15:28:20 -0700

    Add test for downPressed, leftPressed, rightPressed in KeyControl

--- Documents/src/test/java/Documents/main/KeyControlTest.java
+++ Documents/src/test/java/Documents/main/KeyControlTest.java
@@ -29,4 +29,46 @@ public class KeyControlTest {
         keyControl.keyPressed(keyEvent);
         assertFalse(keyControl.upPressed);
     }
+
+    @Test
+    public void testDownKeyPressed() {
+        KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_DOWN, 'D');
+        keyControl.keyPressed(keyEvent);
+        assertTrue(keyControl.downPressed);
+    }
+
+    @Test
+    public void testDownKeyReleased() {
+        KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, 0, 0, KeyEvent.VK_DOWN, 'D');
+        keyControl.keyPressed(keyEvent);
+        assertFalse(keyControl.downPressed);
+    }
+
+    @Test
+    public void testLeftKeyPressed() {
+        KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_LEFT, 'L');
+        keyControl.keyPressed(keyEvent);
+        assertTrue(keyControl.leftPressed);
+    }
+
+    @Test
+    public void testLeftKeyReleased() {
+        KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, 0, 0, KeyEvent.VK_LEFT, 'L');
+        keyControl.keyPressed(keyEvent);
+        assertFalse(keyControl.leftPressed);
+    }
+
+    @Test
+    public void testRightKeyPressed() {
+        KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_RIGHT, 'R');
+        keyControl.keyPressed(keyEvent);
+        assertTrue(keyControl.rightPressed);
+    }
+
+    @Test
+    public void testRightKeyReleased() {
+        KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, 0, 0, KeyEvent.VK_RIGHT, 'R');
+        keyControl.keyPressed(keyEvent);
+        assertFalse(keyControl.rightPressed);
+    }
 }
