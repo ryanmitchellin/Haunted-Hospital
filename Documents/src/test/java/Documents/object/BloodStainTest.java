7cd5a38366f72c7b33685e55f1da4b621db87b3e
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-04-02 14:22:10 -0700

    Fix BloodStainTest bugs into success test

--- Documents/src/test/java/Documents/object/BloodStainTest.java
+++ Documents/src/test/java/Documents/object/BloodStainTest.java
@@ -1,6 +1,5 @@
 package Documents.object;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,17 +7,12 @@ import Documents.main.GamePanel;
 
 public class BloodStainTest {
 
-    private GamePanel gp;
-    private Bloodstain bloodstain;
-
-    @BeforeAll
-    public void setUp() {
-        gp = new GamePanel();
-        bloodstain = new Bloodstain(gp);
-    }
-
     @Test
     public void testConstructor() {
+        GamePanel gp;
+        Bloodstain bloodstain;
+        gp = new GamePanel();
+        bloodstain = new Bloodstain(gp);
         assertEquals("bloodstain", bloodstain.type);
         assertNotNull(bloodstain.img);
     }
