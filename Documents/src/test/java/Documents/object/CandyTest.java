ecad2b1ce10e76cb21e717ae9c51ecb9c86b1b56
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-04-02 14:24:25 -0700

    Fix CandyTest bugs into success test

--- Documents/src/test/java/Documents/object/CandyTest.java
+++ Documents/src/test/java/Documents/object/CandyTest.java
@@ -1,6 +1,5 @@
 package Documents.object;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,17 +7,12 @@ import Documents.main.GamePanel;
 
 public class CandyTest {
 
-    private GamePanel gp;
-    private Candy candy;
-
-    @BeforeAll
-    public void setUp() {
-        gp = new GamePanel();
-        candy = new Candy(gp);
-    }
-
     @Test
     public void testConstructor() {
+        GamePanel gp;
+        Candy candy;
+        gp = new GamePanel();
+        candy = new Candy(gp);
         assertEquals("candy", candy.type);
         assertNotNull(candy.img);
     }
