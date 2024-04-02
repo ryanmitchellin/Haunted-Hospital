9614ddec9a7dd9d3197d08cd821f84ddbb39bf41
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-04-02 14:23:20 -0700

    Fix DoorTest bugs into success test

--- Documents/src/test/java/Documents/object/DoorTest.java
+++ Documents/src/test/java/Documents/object/DoorTest.java
@@ -1,6 +1,5 @@
 package Documents.object;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,17 +7,12 @@ import Documents.main.GamePanel;
 
 public class DoorTest {
 
-    private GamePanel gp;
-    private Door door;
-
-    @BeforeAll
-    public void setUp() {
-        gp = new GamePanel();
-        door = new Door(gp);
-    }
-
     @Test
     public void testConstructor() {
+        GamePanel gp;
+        Door door;
+        gp = new GamePanel();
+        door = new Door(gp);
         assertEquals("door", door.type);
         assertNotNull(door.img);
     }
