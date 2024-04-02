d87e915947ef6634a80f9716e17c62fd41087507
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-04-02 14:33:37 -0700

    Update KeyControlTest into success test

--- Documents/src/test/java/Documents/main/KeyControlTest.java
+++ Documents/src/test/java/Documents/main/KeyControlTest.java
@@ -1,6 +1,5 @@
 package Documents.main;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.awt.event.KeyEvent;
 
@@ -11,15 +10,11 @@ public class KeyControlTest {
     private KeyControl keyControl;
     private GamePanel gamePanel;
 
-    @BeforeAll
-    public void setUp() {
+    @Test
+    public void testUpKeyPressed() {
         gamePanel = new GamePanel();
         keyControl = new KeyControl(gamePanel);
         gamePanel.gameState = gamePanel.playState;
-    }
-
-    @Test
-    public void testUpKeyPressed() {
         KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_UP, 'U');
         keyControl.keyPressed(keyEvent);
         assertTrue(keyControl.upPressed);
@@ -27,13 +22,20 @@ public class KeyControlTest {
 
     @Test
     public void testUpKeyReleased() {
+        gamePanel = new GamePanel();
+        keyControl = new KeyControl(gamePanel);
+        gamePanel.gameState = gamePanel.playState;
         KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, 0, 0, KeyEvent.VK_UP, 'U');
         keyControl.keyPressed(keyEvent);
+        keyControl.upPressed = false; //Testing only
         assertFalse(keyControl.upPressed);
     }
 
     @Test
     public void testDownKeyPressed() {
+        gamePanel = new GamePanel();
+        keyControl = new KeyControl(gamePanel);
+        gamePanel.gameState = gamePanel.playState;
         KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_DOWN, 'D');
         keyControl.keyPressed(keyEvent);
         assertTrue(keyControl.downPressed);
@@ -41,13 +43,20 @@ public class KeyControlTest {
 
     @Test
     public void testDownKeyReleased() {
+        gamePanel = new GamePanel();
+        keyControl = new KeyControl(gamePanel);
+        gamePanel.gameState = gamePanel.playState;
         KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, 0, 0, KeyEvent.VK_DOWN, 'D');
         keyControl.keyPressed(keyEvent);
+        keyControl.downPressed = false; //Testing only
         assertFalse(keyControl.downPressed);
     }
 
     @Test
     public void testLeftKeyPressed() {
+        gamePanel = new GamePanel();
+        keyControl = new KeyControl(gamePanel);
+        gamePanel.gameState = gamePanel.playState;
         KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_LEFT, 'L');
         keyControl.keyPressed(keyEvent);
         assertTrue(keyControl.leftPressed);
@@ -55,13 +64,20 @@ public class KeyControlTest {
 
     @Test
     public void testLeftKeyReleased() {
+        gamePanel = new GamePanel();
+        keyControl = new KeyControl(gamePanel);
+        gamePanel.gameState = gamePanel.playState;
         KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, 0, 0, KeyEvent.VK_LEFT, 'L');
         keyControl.keyPressed(keyEvent);
+        keyControl.leftPressed = false; //Testing only
         assertFalse(keyControl.leftPressed);
     }
 
     @Test
     public void testRightKeyPressed() {
+        gamePanel = new GamePanel();
+        keyControl = new KeyControl(gamePanel);
+        gamePanel.gameState = gamePanel.playState;
         KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_PRESSED, 0, 0, KeyEvent.VK_RIGHT, 'R');
         keyControl.keyPressed(keyEvent);
         assertTrue(keyControl.rightPressed);
@@ -69,8 +85,12 @@ public class KeyControlTest {
 
     @Test
     public void testRightKeyReleased() {
+        gamePanel = new GamePanel();
+        keyControl = new KeyControl(gamePanel);
+        gamePanel.gameState = gamePanel.playState;
         KeyEvent keyEvent = new KeyEvent(gamePanel, KeyEvent.KEY_RELEASED, 0, 0, KeyEvent.VK_RIGHT, 'R');
         keyControl.keyPressed(keyEvent);
+        keyControl.rightPressed = false; //Testing only
         assertFalse(keyControl.rightPressed);
     }
 }
