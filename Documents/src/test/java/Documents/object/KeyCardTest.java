0a6fd8fd60185b7b77b825cd5a40542598294631
rvm1 <ryan_vincent_mitchellin@sfu.ca>
2024-04-02 14:25:53 -0700

    Fix KeyCardTest bugs into success test

--- Documents/src/test/java/Documents/object/KeyCardTest.java
+++ Documents/src/test/java/Documents/object/KeyCardTest.java
@@ -1,6 +1,5 @@
 package Documents.object;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,17 +7,12 @@ import Documents.main.GamePanel;
 
 public class KeyCardTest {
 
-    private GamePanel gp;
-    private KeyCard keyCard;
-
-    @BeforeAll
-    public void setUp() {
-        gp = new GamePanel();
-        keyCard = new KeyCard(gp);
-    }
-
     @Test
     public void testConstructor() {
+        GamePanel gp;
+        KeyCard keyCard;
+        gp = new GamePanel();
+        keyCard = new KeyCard(gp);
         assertEquals("keyCard", keyCard.type);
         assertNotNull(keyCard.img);
     }
