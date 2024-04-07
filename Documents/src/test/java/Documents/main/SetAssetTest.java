293f1519d9bff18f2bb14b67c20e637c15620a8f
Liam Cummings <lfc3@sfu.ca>
2024-04-07 05:43:28 -0700

    wrote some more structural test cases

--- Documents/src/test/java/Documents/main/SetAssetTest.java
+++ Documents/src/test/java/Documents/main/SetAssetTest.java
@@ -43,4 +43,26 @@ public class SetAssetTest {
         assertEquals(true, gp.monster[0] instanceof Ghost);
         assertEquals(true, gp.monster[1] instanceof Ghost);
     }
+
+    @Test
+    public void setTrapsTest(){
+        setAsset.setTraps();
+        for(int i = 10; i<18; i++){
+            assertEquals(true, gp.obj[i] instanceof Bloodstain);
+        }
+    }
+
+    @Test
+    public void setRewardTest(){
+        setAsset.setReward();
+        for(int i = 18; i<21; i++){
+            assertEquals(true, gp.obj[i] instanceof Candy);
+        }
+    }
+
+    @Test
+    public void setDemonTest(){
+        setAsset.setDemon();
+        assertEquals(true, gp.monster[2] instanceof Demon);
+    }
 }
