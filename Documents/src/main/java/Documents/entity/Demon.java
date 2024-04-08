3800d4f85de739e427d3a7a61a49b6527aead8a2
jrm22 <jrm22@sfu.ca>
2024-04-07 22:01:34 -0700

    Simplified randomness bounds in update() for Demon.java

--- Documents/src/main/java/Documents/entity/Demon.java
+++ Documents/src/main/java/Documents/entity/Demon.java
@@ -6,6 +6,10 @@ import java.util.Random;
 
 public class Demon extends Monster {
 
+	public Random random = new Random();
+	public int randomI;
+
+
     public Demon(GamePanel gp) {
 		super(gp);
 		direction = "down";
@@ -45,19 +49,18 @@ public class Demon extends Monster {
 			actionLockCount++;
 
 			if (actionLockCount == 120) {
-				Random random = new Random();
-				int i = random.nextInt(100) + 1;
+				randomI = random.nextInt(100) + 1;
 
-				if (i <= 25) {
+				if (randomI <= 25) {
 					direction = "up";
 				}
-				if (i > 25 && i <= 50) {
+				if (randomI > 25 && randomI <= 50) {
 					direction = "down";
 				}
-				if (i > 50 && i <= 75) {
+				if (randomI > 50 && randomI <= 75) {
 					direction = "left";
 				}
-				if (i > 75 && i <= 100) {
+				if (randomI > 75) {
 					direction = "right";
 				}
 				actionLockCount = 0;
