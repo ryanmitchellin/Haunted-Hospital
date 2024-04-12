436e54160b94d7afa8dae00a740155623efc64a7
jeaohl <jeaohl@sfu.ca>
2024-04-12 02:23:10 -0700

    fixes in the environment setting

--- Documents/src/main/java/Documents/environment/Lighting.java
+++ Documents/src/main/java/Documents/environment/Lighting.java
@@ -7,66 +7,62 @@ import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.awt.Graphics2D;
 
+/**
+ * This class manages the lighting effects in the game by creating a dark filter
+ * with a radial gradient to simulate light around the main character.
+ */
 public class Lighting {
+    private GamePanel gamePanel;
+    private BufferedImage darkFilter;
 
-    GamePanel gamePanel;
-    BufferedImage darkFilter;
-
+    /**
+     * Constructs a Lighting object with a specified GamePanel and circle size for the light.
+     *
+     * @param gamePanel the game panel where the lighting effect is applied
+     * @param circleSize the diameter of the circle of light
+     */
     public Lighting(GamePanel gamePanel, int circleSize) {
+        this.gamePanel = gamePanel;
 
         // Create buffered image
-        darkFilter = new BufferedImage(gamePanel.screenWidth, gamePanel.screenHeight, BufferedImage.TYPE_INT_RGB);
-        Graphics g2 = (Graphics2D) darkFilter.getGraphics();
+        darkFilter = new BufferedImage(gamePanel.screenWidth, gamePanel.screenHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = darkFilter.createGraphics();
 
-        // Create rectangle area
+        // Define the screen area
         Area screenArea = new Area(new Rectangle2D.Double(0, 0, gamePanel.screenWidth, gamePanel.screenHeight));
 
-        // Get the center x and y of the circle of light
-        int centerX = gamePanel.mainCharacter.screenX + (gamePanel.tileSize) / 2;
-        int centerY = gamePanel.mainCharacter.screenY + (gamePanel.tileSize) / 2;
+        // Center coordinates of the light circle
+        int centerX = gamePanel.mainCharacter.screenX + gamePanel.tileSize / 2;
+        int centerY = gamePanel.mainCharacter.screenY + gamePanel.tileSize / 2;
 
-        // Get the top left x and y of the circle of light
-        double x = centerX - gamePanel.screenWidth / 2;
-        double y = centerY - gamePanel.screenHeight / 2;
+        // Top-left coordinates of the circle
+        double x = centerX - circleSize / 2.0;
+        double y = centerY - circleSize / 2.0;
 
-        // Create a light circle shape
+        // Create and subtract the light circle from the screen area
         Shape circleShape = new Ellipse2D.Double(x, y, circleSize, circleSize);
+        screenArea.subtract(new Area(circleShape));
 
-        // Create a light circle area
-        Area lightArea = new Area(circleShape);
-        screenArea.subtract(new Area(lightArea));
+        // Define gradient properties
+        Color[] colors = {new Color(0, 0, 0, 0), new Color(0, 0, 0, 0.25f), new Color(0, 0, 0, 0.5f),
+                          new Color(0, 0, 0, 0.75f), new Color(0, 0, 0, 0.98f)};
+        float[] fractions = {0f, 0.25f, 0.5f, 0.75f, 1f};
+        RadialGradientPaint gradientPaint = new RadialGradientPaint(centerX, centerY, circleSize / 2, fractions, colors);
 
-        Color color[] = new Color[5];
-        float[] fraction = new float[5];
+        g2.setPaint(gradientPaint);
 
-        color[0] = new Color(0,0,0,0f);
-        color[1] = new Color(0,0,0,0.25f);
-        color[2] = new Color(0,0,0,0.5f);
-        color[3] = new Color(0,0,0,0.98f);
-
-        fraction[0] = 0f;
-        fraction[1] = 0.25f;
-        fraction[2] = 0.5f;
-        fraction[3] = 0.75f;
-        fraction[4] = 1f;
-
-        // Create gradian
-        RadialGradientPaint gradientPaint = new RadialGradientPaint(centerX, centerY, (circleSize/2), fraction, color);
-
-        g2.setPaint();
-        // Set colour
-        g2.setColor(new Color(0, 0, 0, 0.95f));
-
-        // Draw the screen without the light area
+        // Fill the area with the gradient to simulate darkness with a lighter area
         g2.fill(screenArea);
-
         g2.dispose();
     }
 
+    /**
+     * Draws the dark filter onto a given Graphics2D surface.
+     *
+     * @param g2 the graphics object used for drawing
+     */
     public void draw(Graphics2D g2) {
         g2.drawImage(darkFilter, 0, 0, null);
-
     }
 }
